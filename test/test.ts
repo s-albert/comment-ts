80b193767eb3c7dc1669fd10bf80d4455de3780c
Albert Strimitzer <koan@live.at>
2018-09-23 08:25:08 +0200

    fix test

--- test/test.ts
+++ test/test.ts
@@ -3,21 +3,17 @@ class X {
   readonly b: string;
   readonly c: boolean;
 
-	/**
-	* Creates an instance of X.
-	* @param X object with default values or dto to initialize the model, including:
-	*	a:	number
-	*	b:	string
-	*	c:	boolean
-	* You may use named params like: new X( { a: <value>, c: <value>,... } )
-	*/
-	constructor( dto: X) {
-		this.a = dto.a;
-		this.b = dto.b;
-		this.c = dto.c;
-	}
-
-
+  /**
+   * Creates an instance of X.
+   * @param X object with default values or dto to initialize the model, including:
+   *	a:	number
+   *	b:	string
+   *	c:	boolean
+   * You may use named params like: new X( { a: <value>, c: <value>,... } )
+   */
+  constructor(dto: X) {
+    this.a = dto.a;
+    this.b = dto.b;
+    this.c = dto.c;
+  }
 }
-
-const test = new X();
